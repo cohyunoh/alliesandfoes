23ad2a1dccb3c231ad1e5020d2e2014e2627aa04
Connor Desktop <connorhyunjin@gmail.com>
2026-03-10 18:47:27 -0400

    Initial commit

---
+++ src/client/java/alliesandfoes/AlliesandfoesClient.java
@@ -0,0 +1,10 @@
+package alliesandfoes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlliesandfoesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/alliesandfoes/AlliesandfoesDataGenerator.java
@@ -0,0 +1,11 @@
+package alliesandfoes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AlliesandfoesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
