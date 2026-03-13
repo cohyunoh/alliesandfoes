7bdbdb7862f42fd81754920c8c54813c610ef563
Connor Desktop <connorhyunjin@gmail.com>
2026-03-13 15:03:19 -0400

    Tried another way to open the start screen, did not work

--- src/main/java/net/cnn_r/alliesandfoes/AlliesandfoesClient.java
+++ src/main/java/net/cnn_r/alliesandfoes/AlliesandfoesClient.java
@@ -4,15 +4,16 @@ import net.cnn_r.alliesandfoes.client.gui.ANFScreenBase;
 import net.cnn_r.alliesandfoes.network.packet.ANFStartScreenS2CPayload;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.network.chat.Component;
 
 public class AlliesandfoesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientPlayNetworking.registerGlobalReceiver(ANFStartScreenS2CPayload.ID, (payload, context) -> {
-            context.client().setScreen(new ANFScreenBase(Component.literal("Start Screen")));
+            ANFScreenBase startscreen = new ANFScreenBase(Component.literal("Start Screen"));
+            context.client().execute(() -> {
+                        context.client().setScreen(startscreen);
+            });
         });
     }
 }
